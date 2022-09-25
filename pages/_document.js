cbb0bccd8b7bf0c53c76c9f933b9c8d7541dcacd
Davide <davide.crisante3@studio.unibo.it>
2022-09-25 22:27:37 +0200

    added Google Analytics script fix 2

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html>
             <Head>
-                <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-                <script dangerouslySetInnerHTML={{
-                    __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){window.dataLayer.push(arguments)}
-                        gtag('js', new Date());
+                <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-                        `}}/>
+                <Script strategy="lazyOnload">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+                </Script>
 
                 <meta charSet="UTF-8" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap"/>
