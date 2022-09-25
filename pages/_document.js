7c94574294589de3c4276a31f0494dd33dd5927a
Davide <davide.crisante3@studio.unibo.it>
2022-09-25 22:29:10 +0200

    added Google Analytics script fix 3

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
             <Head>
                 <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-                <Script strategy="lazyOnload">
+                <Script strategy="lazyOnload" id={"google-analytics-script"}>
                     {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
