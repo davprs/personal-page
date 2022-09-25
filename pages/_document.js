e1af7dac1524e52eb90a3d8471d16a4afcf7574b
Davide <davide.crisante3@studio.unibo.it>
2022-09-25 22:18:11 +0200

    added Google Analytics script fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,15 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <!-- Google tag (gtag.js) -->
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-76B3GRGF6B"></script>
-                <script>
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments)}
+                        gtag('js', new Date());
 
-                    gtag('config', 'G-76B3GRGF6B');
-                </script>
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                        `}}/>
 
                 <meta charSet="UTF-8" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap"/>
