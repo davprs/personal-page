b4df0ae006eb91167d93653c611b9d10d8259720
Davide <davide.crisante3@studio.unibo.it>
2022-09-25 22:01:18 +0200

    added Google Analytics script

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,16 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <!-- Google tag (gtag.js) -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-76B3GRGF6B"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-76B3GRGF6B');
+                </script>
+
                 <meta charSet="UTF-8" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap"/>
             </Head>
